f0e638f1e7cad41f8b1c9279dec0a28229bf3664
Jaroslav Týc <mail@jaroslavtyc.com>
2018-02-21 10:37:14 +0100

    Fatigue from all destruction types covered

--- parts/destruction.php
+++ parts/destruction.php
@@ -5,25 +5,64 @@ use \DrdPlus\Tables\Measurements\Time\Exceptions\CanNotConvertThatBonusToTime;
 
 /** @var Controller $controller */
 ?>
+    <hr>
     <label>Materiál
         <select name="material">
             <?php foreach ($controller->getMaterialCodes() as $materialCode) { ?>
-                <option value="<?= $materialCode ?>"><?= $materialCode->getValue() ?>
+                <option value="<?= $materialCode ?>"
+                        <?php if ($controller->getSelectedMaterial()->getValue() === $materialCode->getValue()) { ?>selected="selected"
+                    <?php } ?>><?= $materialCode->getValue() ?>
                     (pevnost <?= $controller->getMaterialResistance($materialCode) ?>)
                 </option>
             <?php } ?>
         </select>
     </label>
-
+    <hr>
     <h4>Něco obyčejného</h4>
     <div class="example">
         běžný stůl, dlaždice, meč, lopata...
     </div>
+    <?php $realTimeOfBasicItemDestruction = $controller->getRealTimeOfBasicItemDestruction();
+$fatigueFromBasicItemDestruction = null;
+try {
+    $fatigueFromBasicItemDestruction = $realTimeOfBasicItemDestruction->getFatigue();
+    ?>
+    <label>Velikost věci <input type="number" value="<?= $controller->getSelectedItemSize() ?>"
+                                name="<?= $controller::ITEM_SIZE ?>"></label>
+    <div>
+        Únava <?= $fatigueFromBasicItemDestruction->getValue() ?>
+    </div>
+    <?php
+} catch (CanNotConvertThatBonusToTime $canNotConvertThatBonusToTime) {
+    ?>
+    <div class="error">
+        Únava mimo rozsah
+    </div>
+<?php } ?>
+    <hr>
 
     <h4>Tvarem připomínajícím tělo</h4>
     <div class="example">
         socha, krápník, rampouch, sloup...
     </div>
+    <label>Velikost "těla" <input type="number" value="<?= $controller->getSelectedBodySize() ?>"
+                                  name="<?= $controller::BODY_SIZE ?>"></label>
+    <?php $realTimeOfStatueLikeDestruction = $controller->getRealTimeOfStatueLikeDestruction();
+$fatigueFromStatueLikeDestruction = null;
+try {
+    $fatigueFromStatueLikeDestruction = $realTimeOfStatueLikeDestruction->getFatigue();
+    ?>
+    <div>
+        Únava <?= $fatigueFromStatueLikeDestruction->getValue() ?>
+    </div>
+    <?php
+} catch (CanNotConvertThatBonusToTime $canNotConvertThatBonusToTime) {
+    ?>
+    <div class="error">
+        Únava mimo rozsah
+    </div>
+<?php } ?>
+    <hr>
 
     <h4>Něco tlustšího</h4>
     <div class="example">
