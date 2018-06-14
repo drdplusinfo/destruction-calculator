942091a752b050feb7db6b3a65fc6448c702568e
Jaroslav Týc <mail@jaroslavtyc.com>
2018-06-14 18:21:39 +0200

    Page name clarified

---
+++ DrdPlus/Tests/tests_config.php
@@ -0,0 +1,8 @@
+<?php
+global $testsConfiguration;
+$testsConfiguration = new \DrdPlus\Tests\FrontendSkeleton\TestsConfiguration();
+$testsConfiguration->disableHasCustomBodyContent();
+$testsConfiguration->disableHasTables();
+$testsConfiguration->setExpectedWebName('DrD+ kalkulátor ničení');
+$testsConfiguration->setExpectedPageTitle('DrD+ kalkulátor ničení');
+$testsConfiguration->disableHasMoreVersions();
